d203df65071a924a3598a41b1d74499f007e9a4c
khanhpl <khanh.phanluu01@gmail.com>
2022-02-09 14:05:02 +0700

    khanhpl 9-2 update cusmainscreen add slide ads

--- Pegoda Mobile/Pegoda/lib/screens/customer/cus_home_screen.dart
+++ Pegoda Mobile/Pegoda/lib/screens/customer/cus_home_screen.dart
@@ -1,7 +1,10 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pegoda/screens/customer/up_nav_bar.dart';
 import '../../MyLib/constants.dart' as Constants;
+
+import 'package:carousel_pro/carousel_pro.dart';
 class CusHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,7 +13,49 @@ class CusHomeScreen extends StatelessWidget {
     var _bgColor = Constants.bgColor;
     return Scaffold(
       appBar: UpAppBar(context),
-      body: Material(),
+      body: Material(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(_pageWidth*0.03, _pageHeight*0.03, _pageWidth*0.03, _pageHeight*0.1),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: _pageHeight * 0.2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: CaroselImage,
+                ),
+                SizedBox(height: _pageHeight*0.03),
+
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+Widget CaroselImage = Center(
+  child: Carousel(
+    images: [
+      AssetImage('assets/cus/main_screen/slide1.jpg'),
+      AssetImage('assets/cus/main_screen/slide2.jpg'),
+      AssetImage('assets/cus/main_screen/slide3.jpg'),
+      AssetImage('assets/cus/main_screen/slide4.png'),
+      AssetImage('assets/cus/main_screen/slide5.jpg'),
+    ],
+    autoplay: true,
+    dotSize: 5,
+    dotSpacing: 30,
+    indicatorBgPadding: 0,
+    autoplayDuration: Duration(seconds: 5),
+    borderRadius: true,
+    dotBgColor: Colors.black.withOpacity(0),
+  ),
+);
