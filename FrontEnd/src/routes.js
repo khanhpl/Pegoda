ef86936e5f1f4f22e45b42e22356885b7ab670a4
Le Huu Hieu <01643815296h@gmail.com>
2022-02-22 08:52:04 +0700

    hieu/PFE-2 1

--- FrontEnd/src/routes.js
+++ FrontEnd/src/routes.js
@@ -1,15 +1,15 @@
-import { Navigate, useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom'
 // layouts
-import DashboardLayout from './layouts/dashboard';
-import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+import DashboardLayout from './layouts/dashboard'
+import LogoOnlyLayout from './layouts/LogoOnlyLayout'
 //
-import Login from './pages/Login';
-import Register from './pages/Register';
-import DashboardApp from './pages/DashboardApp';
-import Products from './pages/Products';
-import Blog from './pages/Blog';
-import User from './pages/User';
-import NotFound from './pages/Page404';
+import Login from './pages/Login'
+import Register from './pages/Register'
+import DashboardApp from './pages/DashboardApp'
+import Products from './pages/Products'
+import User from './pages/User'
+import NotFound from './pages/Page404'
+import Order from './pages/Order'
 
 // ----------------------------------------------------------------------
 
@@ -23,7 +23,7 @@ export default function Router() {
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
         { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> }
+        { path: 'order', element: <Order /> }
       ]
     },
     {
@@ -38,5 +38,5 @@ export default function Router() {
       ]
     },
     { path: '*', element: <Navigate to="/404" replace /> }
-  ]);
+  ])
 }
