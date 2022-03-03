f18a85bc66bb2288b37e8adc58ab7af389918d40
voxquoocshuyy <66914322+voxquoocshuyy@users.noreply.github.com>
2022-03-03 13:36:41 +0700

    huy/PBE-24

---
+++ Api/Repositories/IServiceTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Entities;
+
+namespace Api.Repositories
+{
+    public interface IServiceTypeRepository<T>
+    {
+        Task<ServiceType> Create(ServiceType serviceType);
+        Task<bool> Update(ServiceType newServiceType);
+        Task<ServiceType> GetById(Guid id);
+        List<ServiceType> GetList(int pageNumber, int pageSize);
+        Task<bool> Delete(Guid id);
+    }
+}
