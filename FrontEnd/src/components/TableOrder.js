6aee4249c461491f960f4d808e7fc85c02eff238
Le Huu Hieu <01643815296h@gmail.com>
2022-03-21 09:10:28 +0700

    hieu/PFE-14 v4

--- FrontEnd/src/components/TableOrder.js
+++ FrontEnd/src/components/TableOrder.js
@@ -1,11 +1,12 @@
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Box, CircularProgress, ButtonGroup, Button } from "@mui/material"
-import { Delete, Edit, InfoOutlined } from '@mui/icons-material'
-import { useEffect, useState } from "react"
+import { Close, InfoOutlined } from '@mui/icons-material'
+import { Box, Button, ButtonGroup, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
 import axios from "axios"
+import { useEffect, useState } from "react"
 
 const TableOrder = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
+    const [openDialog, setOpenDialog] = useState(false)
 
     const token = localStorage.getItem('token')
     const centerId = localStorage.getItem('centerId')
@@ -21,56 +22,114 @@ const TableOrder = () => {
             .catch(error => console.log(error))
     }, [token, centerId, apiUrl])
 
+    const handleCloseDialog = () => {
+        setOpenDialog(false)
+    }
+
+    const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+        '& .MuiDialogContent-root': {
+            padding: theme.spacing(2),
+        },
+        '& .MuiDialogActions-root': {
+            padding: theme.spacing(1),
+        },
+    }))
+
     return (
         <>
             {loading
                 ? <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                     <CircularProgress />
                 </Box>
-                : <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>STT</TableCell>
-                                <TableCell>Tên khách hàng</TableCell>
-                                <TableCell align="right">Tên Pet</TableCell>
-                                <TableCell align="right">Giới tính Pet</TableCell>
-                                <TableCell align="right">Tên dịch vụ</TableCell>
-                                <TableCell align="right">Thời gian đặt</TableCell>
-                                <TableCell align="right">Trạng thái</TableCell>
-                                <TableCell align="right">Hành động</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data.map((row, index) => (
-                                <TableRow
-                                    key={index}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {index + 1}
-                                    </TableCell>
-                                    <TableCell>{row.customerName}</TableCell>
-                                    <TableCell align="right">{row.petName}</TableCell>
-                                    <TableCell align="right">{row.petGender}</TableCell>
-                                    <TableCell align="right">{row.serviceName}</TableCell>
-                                    <TableCell align="right">{row.bookingTime.slice(0, 19).replace('T', '  ')}</TableCell>
-                                    <TableCell align="right">
-                                        <ButtonGroup>
-                                            <Button variant="contained" color="secondary">Xác Nhận</Button>
-                                            <Button variant="outlined" color="error">Huỷ</Button>
-                                        </ButtonGroup>
-                                    </TableCell>
-                                    <TableCell align='right'>
-                                        <InfoOutlined color='primary' style={{ marginRight: 10, cursor: 'pointer' }} onClick={() => console.log('info')} />
-                                        {/* <Edit color="info" style={{ marginRight: 10, cursor: 'pointer' }} onClick={() => { console.log('edit') }} />
-                                        <Delete color="error" style={{ cursor: 'pointer' }} onClick={() => { console.log('delete') }} /> */}
-                                    </TableCell>
+                :
+                <>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>STT</TableCell>
+                                    <TableCell align="right">Tên khách hàng</TableCell>
+                                    <TableCell align="right">Tên Pet</TableCell>
+                                    <TableCell align="right">Giới tính Pet</TableCell>
+                                    <TableCell align="right">Thời gian đặt</TableCell>
+                                    <TableCell align="right">Trạng thái</TableCell>
+                                    <TableCell align="right">Hành động</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                            </TableHead>
+                            <TableBody>
+                                {data.map((row, index) => (
+                                    <TableRow
+                                        key={index}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {index + 1}
+                                        </TableCell>
+                                        <TableCell align="right">{row.customerName}</TableCell>
+                                        <TableCell align="right">{row.petName}</TableCell>
+                                        <TableCell align="right">{row.gender}</TableCell>
+                                        <TableCell align="right">{row.date.slice(0, 19).replace('T', '  ')}</TableCell>
+                                        <TableCell align="right">
+                                            <ButtonGroup>
+                                                <Button variant="contained" color="secondary">Xác Nhận</Button>
+                                                <Button variant="outlined" color="error">Huỷ</Button>
+                                            </ButtonGroup>
+                                        </TableCell>
+                                        <TableCell align='right'>
+                                            <InfoOutlined color='primary' style={{ marginRight: 10, cursor: 'pointer' }} onClick={() => {
+                                                console.log(row.orderId)
+                                                // setOpenDialog(true)
+                                            }} />
+                                            {/* <Edit color="info" style={{ marginRight: 10, cursor: 'pointer' }} onClick={() => { console.log('edit') }} />
+                                        <Delete color="error" style={{ cursor: 'pointer' }} onClick={() => { console.log('delete') }} /> */}
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <BootstrapDialog
+                        aria-labelledby="customized-dialog-title"
+                        open={openDialog}
+                    >
+                        <DialogTitle sx={{ m: 0, p: 2 }}>
+                            Chi tiết đơn hàng
+                            <IconButton
+                                aria-label="close"
+                                onClick={handleCloseDialog}
+                                sx={{
+                                    position: 'absolute',
+                                    right: 8,
+                                    top: 8,
+                                    color: (theme) => theme.palette.grey[500],
+                                }}
+                            >
+                                <Close />
+                            </IconButton>
+                        </DialogTitle>
+                        <DialogContent dividers>
+                            <Typography gutterBottom>
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                                consectetur ac, vestibulum at eros.
+                            </Typography>
+                            <Typography gutterBottom>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                                Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                            </Typography>
+                            <Typography gutterBottom>
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+                                magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+                                ullamcorper nulla non metus auctor fringilla.
+                            </Typography>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleCloseDialog}>
+                                Lưu thay đổi
+                            </Button>
+                        </DialogActions>
+                    </BootstrapDialog>
+                </>
             }
         </>
     )
