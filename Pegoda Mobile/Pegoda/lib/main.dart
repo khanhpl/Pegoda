9904a717cfc7e3bae60b427b28beb818e172aff0
DanielHieu <47274268+DanielHieu@users.noreply.github.com>
2022-03-21 16:20:30 +0700

    push notify mobile

--- Pegoda Mobile/Pegoda/lib/main.dart
+++ Pegoda Mobile/Pegoda/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:pegoda/MyLib/provider/google_sign_in_provider.dart';
 import 'package:pegoda/controllers/customer_main.dart';
@@ -16,41 +17,53 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:pegoda/screens/welcome/register_screen.dart';
 import 'package:pegoda/screens/customer/cus_main/chat_screen.dart';
 
+import 'models/local_notification.dart';
+
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  LocalNotificationService.initilize();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => GoogleSignInProvider(),
-        child: MaterialApp(
-          initialRoute: '/',
-          routes: <String, WidgetBuilder>{
-            '/': (context) => WelcomeScreen(),
-            '/cusMain': (context) =>
-                CusMain(selectedIndex: 0, isBottomNav: true),
-            '/searchScreen': (context) => SearchScreen(),
-            '/cusAccountScreen': (context) =>
-                CusMain(selectedIndex: 3, isBottomNav: false),
-            '/petScreen': (context) =>
-                CusMain(selectedIndex: 1, isBottomNav: true),
-            '/notificationScreen': (context) =>
-                CusMain(selectedIndex: 1, isBottomNav: false),
-            '/chatScreen': (context) => ChatPage(),
-            '/personalSettingScreen': (context) => PersonalSettingScreen(),
-            '/orderHistoryScreen': (context) => OrderHistoryScreen(),
-            '/appInforScreen': (context) => AppInforScreen(),
-            '/appSettingScreen': (context) => AppSettingScreen(),
-            '/questionScreen': (context) => QuestionsScreen(),
-            '/addPetScreen': (context) => AddPetScreen(),
-            '/neareastPCCScreen': (context) => NearestPCCScreen(),
-            '/orderScreen': (context) => OrderScreen(),
-          },
-          // home: WelcomeScreen(),
-        ),
-      );
+    create: (context) => GoogleSignInProvider(),
+    child: MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => CusMain(selectedIndex: 0, isBottomNav: true),
+        '/cusMain': (context) =>
+            CusMain(selectedIndex: 0, isBottomNav: true),
+        '/searchScreen': (context) => SearchScreen(),
+        '/cusAccountScreen': (context) =>
+            CusMain(selectedIndex: 3, isBottomNav: false),
+        '/petScreen': (context) =>
+            CusMain(selectedIndex: 1, isBottomNav: true),
+        '/notificationScreen': (context) =>
+            CusMain(selectedIndex: 2, isBottomNav: true),
+        '/chatScreen': (context) => ChatPage(),
+        '/personalSettingScreen': (context) => PersonalSettingScreen(),
+        '/orderHistoryScreen': (context) => OrderHistoryScreen(),
+        '/appInforScreen': (context) => AppInforScreen(),
+        '/appSettingScreen': (context) => AppSettingScreen(),
+        '/questionScreen': (context) => QuestionsScreen(),
+        '/addPetScreen': (context) => AddPetScreen(),
+        '/neareastPCCScreen': (context) => NearestPCCScreen(),
+        '/orderScreen': (context) => OrderScreen(),
+      },
+      // home: WelcomeScreen(),
+    ),
+  );
 }
--- Pegoda Mobile/Pegoda/lib/screens/customer/notification/notification_screen.dart
+++ Pegoda Mobile/Pegoda/lib/screens/customer/notification/notification_screen.dart
@@ -1,7 +1,36 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import '../../../MyLib/constants.dart' as Constants;
+import '../../../main.dart';
 
-class NotificationScreen extends StatelessWidget {
+class NotificationScreen extends StatefulWidget {
+
+  final List<RemoteMessage> messages;
+  NotificationScreen({Key? key, required this.messages}) : super (key:key);
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  String notificationMsg = "Waiting for notifcation";
+  List<RemoteMessage> msg = [];
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    msg = widget.messages;
+    FirebaseMessaging.onMessage.listen((event) {
+      if (mounted){
+        setState(() {
+          msg = [...msg,event];
+        });
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     var _pageHeight = MediaQuery.of(context).size.height;
@@ -37,6 +66,39 @@ class NotificationScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: msg.length,
+                    itemBuilder: (context, index) {
+                      RemoteMessage message = msg[index];
+                      var sendTime = DateTime.parse(message.sentTime.toString()).compareTo(DateTime.now()) * -1;
+                      return Card(
+                        child: ListTile(
+                          leading: Container(
+                            child: Image.asset(
+                              'assets/Pegoda.png',
+                            ),
+                          ),
+                          title: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  message.notification?.title ?? 'no RemoteMessage. available',style: TextStyle(
+                                    fontWeight: FontWeight.bold
+                                ),),
+                              ),
+                              Text(sendTime.toString() + " gio truoc")
+                            ],
+
+                          ),
+                          subtitle:
+                          Text(message.notification?.body ?? 'no data'),
+                          onTap: (){},
+                          isThreeLine: true,
+                          trailing: Icon(Icons.more_horiz),
+                        ),
+                      );
+                    }),
               ],
             ),
           ),
