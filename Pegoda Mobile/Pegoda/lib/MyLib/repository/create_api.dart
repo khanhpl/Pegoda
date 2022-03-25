a69249cd0dc79bf5f362db44724c673d8549675f
Le Huu Hieu <01643815296h@gmail.com>
2022-03-25 13:19:35 +0700

    hieu/PBM-3

--- Pegoda Mobile/Pegoda/lib/MyLib/repository/create_api.dart
+++ Pegoda Mobile/Pegoda/lib/MyLib/repository/create_api.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'package:jwt_decode/jwt_decode.dart';
 import 'package:http/http.dart' as http;
+import 'package:signalr_netcore/hub_connection.dart';
+import 'package:signalr_netcore/hub_connection_builder.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
-// import 'package:signalr_client/signalr_client.dart';
 import '../globals.dart' as Globals;
 
 class CreateApi {
@@ -10,59 +11,88 @@ class CreateApi {
       String date, String totalPrice, String centerId, String petId) async {
     try {
       var url = Uri.parse("https://pegoda.azurewebsites.net/api/v1.0/orders");
-      // final response = await http.post(
-      //   url,
-      //   headers: <String, String>{
-      //     'Content-Type': 'application/json; charset=UTF-8',
-      //   },
-      //   body: jsonEncode(
-      //     <String, String>{
-      //       'date': date,
-      //       'totalPrice': totalPrice,
-      //       'status': 'pending',
-      //       'scores': '0',
-      //       'feedback': '0',
-      //       'deviceId': Globals.deviceToken,
-      //       'centerId': centerId,
-      //       'petId': petId
-      //     },
-      //   ),
-      // );
-      // if (response.statusCode.toString() == '201') {
-      // try {
-      //   const serverUrl = 'https://pegoda.azurewebsites.net/chatHub';
-      //   print('pending..');
-      //   final hubConnection = HubConnectionBuilder().withUrl(serverUrl).build();
-      //   print(hubConnection.state);
-      //   hubConnection.serverTimeoutInMilliseconds = 10 * 60 * 60 * 1000;
-      //   hubConnection.keepAliveIntervalInMilliseconds = 10 * 60 * 60 * 1000;
-      //   hubConnection.state == HubConnectionState.Disconnected
-      //       ? await hubConnection
-      //           .start()
-      //           ?.catchError((onError) => print(onError))
-      //       : await hubConnection.stop();
-      //   print(hubConnection.state);
-      //   print('Connected!');
-      //   // print(hubConnection.state);
-      //   // final result =
-      //   //     await hubConnection.invoke("Request", args: <Object>["order"]);
-      //   // print(result);
-      // } on Exception catch (e) {
-      //   print(e);
-      // }
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(
+          <String, String>{
+            'date': date,
+            'totalPrice': totalPrice,
+            'status': 'pending',
+            'scores': '0',
+            'feedback': '0',
+            'deviceId': Globals.deviceToken,
+            'centerId': centerId,
+            'petId': petId
+          },
+        ),
+      );
+      print(response.body);
+      if (response.statusCode.toString() == '201') {
+        try {
+          const serverUrl = 'https://pegoda.azurewebsites.net/chatHub';
+          print('pending..');
+          final hubConnection = HubConnectionBuilder()
+              .withUrl(serverUrl)
+              .withAutomaticReconnect()
+              .build();
+          if (hubConnection.state == HubConnectionState.Disconnected) {
+            await hubConnection
+                .start()
+                ?.then((_) => print('Connected!'))
+                .catchError((onError) => print(onError));
+          }
+          print(hubConnection.state);
+          await hubConnection
+              .invoke("Request")
+              .then((value) => print('invoke oke'))
+              .catchError((onError) => print(onError));
+        } on Exception catch (e) {
+          print(e);
+        }
 
-      IO.Socket socket = IO.io('https://pegoda.azurewebsites.net/chatHub');
-      socket.onConnect((_) {
-        print('connected');
-        socket.once("Request", (data) {
-          print(data);
-        });
-      });
+        // const serverUrl = 'https://pegoda.azurewebsites.net/chatHub';
+        // print('pending..');
+        // final hubConnection = HubConnectionBuilder()
+        //     .withUrl(serverUrl)
+        //     .withAutomaticReconnect()
+        //     .build();
+        // if (hubConnection.state == HubConnectionState.Disconnected) {
+        //   await hubConnection
+        //       .start()
+        //       ?.then((_) => print('Connected!'))
+        //       .catchError((onError) => print(onError));
+        // }
+        // print(hubConnection.state);
+        // // if (hubConnection.state == HubConnectionState.Connected) {
+        // //   await hubConnection.invoke("Request", args: <Object>["order"]);
+        // print('invoke success');
+        // }
 
-      return true;
-      // } else {
-      //   return false;
-      // }
+        // IO.Socket socket =
+        //     IO.io('https://pegoda.azurewebsites.net/chatHub', <String, dynamic>{
+        //   'autoConnect': true,
+        // });
+        // socket.connect();
+        // socket.clearListeners();
+        // socket.onConnect((_) {
+        //   print('connected!');
+        //   socket.emit('Request', 'order');
+        // });
+        // print('test1123');
+        // socket.emit('Request', 'order');
+
+        // socket.onDisconnect((_) => print('disconnected'));
+
+        return true;
+      } else {
+        return false;
+      }
+    } on Exception catch (e) {
+      print(e);
+      return false;
     } finally {}
   }
 
